406a079febec4d58b8666b13495298d7b8b268fe
md-ahmodullah <contactme.ahmod@gmail.com>
2024-10-04 12:15:52 +0600

    Modal is on progress

--- js/script.js
+++ js/script.js
@@ -1,86 +1,106 @@
 // Catch DOM Element
-const categoryEl = document.getElementById('category-container');
+const categoryEl = document.getElementById("category-container");
 
 // time converter function
 const toStringDateTime = (time) => {
-    const hours = parseInt(time / 3600);
-    let remainingSec = time % 3600;
-    const minutes = parseInt(remainingSec / 60);
-    remainingSec = remainingSec % 60
-    return `${hours}h ${minutes}m ${remainingSec}s ago`;
-}
+  const hours = parseInt(time / 3600);
+  let remainingSec = time % 3600;
+  const minutes = parseInt(remainingSec / 60);
+  remainingSec = remainingSec % 60;
+  return `${hours}h ${minutes}m ${remainingSec}s ago`;
+};
 
 // remove all active color function
 const removeColor = () => {
-    const btnClass = document.getElementsByClassName('category-btn');
-    for (const btn of btnClass) {
-        btn.classList.remove('active')
-    }
-}
-
+  const btnClass = document.getElementsByClassName("category-btn");
+  for (const btn of btnClass) {
+    btn.classList.remove("active");
+  }
+};
 
 // Load and fetch Categories
-const loadCategories = async() => {
-    try {
-        const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/categories');
-        const data = await res.json();
-        displayCategories(data.categories);
-        
-    } catch (error) {
-        console.log(`ERROR : ${error}`);
-    }
-}
+const loadCategories = async () => {
+  try {
+    const res = await fetch(
+      "https://openapi.programming-hero.com/api/phero-tube/categories"
+    );
+    const data = await res.json();
+    displayCategories(data.categories);
+  } catch (error) {
+    console.log(`ERROR : ${error}`);
+  }
+};
 
 // Load and fetch category videos
-const loadCategoryVideos = async(id) => {
-    try {
-        const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`);
-        const data = await res.json();
-        removeColor();
-        const activeButton = document.getElementById(`btn-${id}`);
-        activeButton.classList.add("active");
-        
-        displayVideos(data.category);
-        
-    } catch (error) {
-        console.log(`ERROR : ${error}`);
-    }
-}
+const loadCategoryVideos = async (id) => {
+  try {
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/phero-tube/category/${id}`
+    );
+    const data = await res.json();
+    removeColor();
+    const activeButton = document.getElementById(`btn-${id}`);
+    activeButton.classList.add("active");
+
+    displayVideos(data.category);
+  } catch (error) {
+    console.log(`ERROR : ${error}`);
+  }
+};
 
 // display Categories
 const displayCategories = (data) => {
-    data.map((categoryList) => {
-        const category = categoryList.category;
-        // create button
-        const buttonContainer = document.createElement('div');
-        buttonContainer.innerHTML = 
-        `
+  data.map((categoryList) => {
+    const category = categoryList.category;
+    // create button
+    const buttonContainer = document.createElement("div");
+    buttonContainer.innerHTML = `
         <button id="btn-${categoryList.category_id}" onclick="loadCategoryVideos(${categoryList.category_id})" class="btn category-btn">${category}</button>
         `;
 
-        // add button to container
-        categoryEl.appendChild(buttonContainer);
-    })
-}
+    // add button to container
+    categoryEl.appendChild(buttonContainer);
+  });
+};
 
 // Load and fetch videos
-const loadVideos = async() => {
-    try {
-        const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/videos');
-        const data = await res.json();
-        displayVideos(data.videos);
-    } catch (error) {
-        console.log(`ERROR : ${error}`);
-    }
-}
+const loadVideos = async () => {
+  try {
+    const res = await fetch(
+      "https://openapi.programming-hero.com/api/phero-tube/videos"
+    );
+    const data = await res.json();
+    displayVideos(data.videos);
+  } catch (error) {
+    console.log(`ERROR : ${error}`);
+  }
+};
+
+// Details Button
+const loadDetails = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phero-tube/video/${id}`
+  );
+  const eachVideo = await res.json();
+  displayDetails(eachVideo);
+};
+
+// Display Details
+const displayDetails = (eachVideos) => {
+  const ID = eachVideos.video.video_id;
+  const button = document.getElementById(ID);
+  button.addEventListener("click", () => {
+    alert("You have done Well");
+  });
+};
 
 const displayVideos = (videos) => {
-    const videoContainer = document.getElementById('videos');
-    videoContainer.innerHTML = '';
+  const videoContainer = document.getElementById("videos");
+  videoContainer.innerHTML = "";
 
-    if (videos.length == 0) {
-        videoContainer.classList.remove('grid')
-        videoContainer.innerHTML = `
+  if (videos.length == 0) {
+    videoContainer.classList.remove("grid");
+    videoContainer.innerHTML = `
             <div class="min-h-[300px] flex flex-col justify-center items-center gap-5">
                 <img src="./assets/Icon.png" />
                 <h2 class="text-3xl text-black font-extrabold">
@@ -88,49 +108,70 @@ const displayVideos = (videos) => {
                 </h2>
             </div>
             `;
-        return
-    }
-    else{
-        videoContainer.classList.add('grid')
-    }
+    return;
+  } else {
+    videoContainer.classList.add("grid");
+  }
 
-    videos.map((video) => {
-        const card = document.createElement('div');
-        card.classList = "card card-compact";
-        const cardElement = 
-        `
+  videos.map((video) => {
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    const cardElement = `
         <figure class="h-[200px] relative">
             <img
             src=${video.thumbnail}
             class="h-full w-full object-cover"
             alt="Shoes" />
             ${
-                video.others?.posted_date.length == 0 ? '' : `<span class="text-xs bg-black rounded-lg text-white p-2 absolute right-2 bottom-2">${toStringDateTime(video.others?.posted_date)}</span>`
+              video.others?.posted_date.length == 0
+                ? ""
+                : `<span class="text-xs bg-black rounded-lg text-white p-2 absolute right-2 bottom-2">${toStringDateTime(
+                    video.others?.posted_date
+                  )}</span>`
             }
         </figure>
-        <div class="px-0 py-2 flex gap-2">
+        <div class="px-0 py-2 flex gap-2 items-center">
             <div>
-                <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture} />
+                <img class="w-10 h-10 rounded-full object-cover" src=${
+                  video.authors[0].profile_picture
+                } />
             </div>
-            <div>
-                <h2 class="font-bold">${video.title}</h2>
-                <div class="flex items-center gap-1">
-                    <p class="text-gray-400">${video.authors[0].profile_name}</p>
-                    ${
-                        video.authors[0].verified == true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />` : ''
-                    }
-                    
+            <div class="flex items-end gap-3">
+                <div>
+                    <h2 class="font-bold">${video.title}</h2>
+                    <div class="flex gap-1">
+                        <p class="text-gray-400">${
+                          video.authors[0].profile_name
+                        }</p>
+                        ${
+                          video.authors[0].verified == true
+                            ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />`
+                            : ""
+                        }
+                    </div>
                 </div>
-                <p></p>
+                <div><button class="btn" onclick="loadDetails('${
+                  video.video_id
+                }')" id="${video.video_id}">View Detail</button>
+                <dialog id="${video.video_id}" class="modal">
+                    <div class="modal-box">
+                    <h3 class="text-lg font-bold">Hello!</h3>
+                    <p class="py-4">Press ESC key or click outside to close</p>
+                    </div>
+                    <form method="dialog" class="modal-backdrop">
+                    <button>close</button>
+                    </form>
+                </dialog>
+                </div>
             </div>
             
         </div>
         `;
-        card.innerHTML = cardElement;
+    card.innerHTML = cardElement;
 
-        videoContainer.appendChild(card)
-    })
-}
+    videoContainer.appendChild(card);
+  });
+};
 
 loadCategories();
 loadVideos();
